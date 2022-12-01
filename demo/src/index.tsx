36ecfbc66576c07eb51434c961aea999b6bcdbeb
AKA MKII <58581967+AKAMKII6666@users.noreply.github.com>
2022-12-01 18:53:43 +0800

    updatedbybobliao

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,25 +1,14 @@
 /*** examples/src/app.js ***/
 import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
-import web3express from "../../lib/index"; // 引入组件
-const { walletContext, useWallet, commonContext, useCommon, Button } = web3express;
-import ExcOne from "./excOne";
+import all from "../../lib/index";
 
 const App = () => {
-	//初始化commonHook
-	const commonObject = useCommon("development");
-	//初始化钱包操作器
-	const walletHalper = useWallet(false, "development");
-
+	console.log(all);
 	return (
 		<>
 			<div>
-				<commonContext.Provider value={commonObject}>
-					<walletContext.Provider value={walletHalper}>
-						<ExcOne></ExcOne>
-					</walletContext.Provider>
-				</commonContext.Provider>
-				<Button>asdasdsad</Button>
+				<button>asdasdsad</button>
 			</div>
 		</>
 	);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import resoHook from "./resoHook";
+export type { Ireso, EresoMode, Iconfig, IconfigMutiple, Imq, EscreenState, queryItem } from "./iReso";
+import { _mobileAdp as mobileAdp } from "./mobileAdp";
+
+export default { resoHook, mobileAdp };
