f8555df44e0238a823a91c5d993e516c3868625f
AKA MKII <58581967+AKAMKII6666@users.noreply.github.com>
2022-12-02 04:08:12 +0800

    bobliao

--- dist/resoHook.d.ts
+++
@@ -1,5 +0,0 @@
-import { Iconfig, Ireso, IconfigMutiple } from './iReso';
-import { _mobileAdp } from './mobileAdp';
-export declare type config = Iconfig | IconfigMutiple;
-declare const useReso: (config?: config) => Ireso;
-export { useReso as default, _mobileAdp };
