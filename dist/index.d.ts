622bfa6395ce62fa519887c300a7268a0cdb08e3
AKA MKII <58581967+AKAMKII6666@users.noreply.github.com>
2023-03-27 16:04:10 +0800

    ud

---
+++ dist/index.d.ts
@@ -0,0 +1,33 @@
+import useReso, { _mobileAdp, resoContext, useResoContext } from './resoHook';
+export type { Ireso, Iconfig, IconfigMutiple, Imq, queryItem } from './iReso';
+/**
+ * 屏幕状态
+ */
+export declare enum EscreenState {
+    /**
+     * 横向屏幕
+     */
+    HORIZONTAL = "h",
+    /**
+     * 竖屏显示
+     */
+    VERTICAL = "v"
+}
+/**
+ * 设置分辨率适配器的工作模式
+ */
+export declare enum EresoMode {
+    /**
+     * auto:自动选择高度还是宽度来调整
+     */
+    AUTO = "auto",
+    /**
+     * width:只通过宽度调整
+     */
+    WIDTH = "width",
+    /**
+     * height:只通过高度调整
+     */
+    HEIGHT = "height"
+}
+export { useReso, _mobileAdp, resoContext, useResoContext };
